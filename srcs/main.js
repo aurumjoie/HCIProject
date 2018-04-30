ad7773db51f79e2ff9d3a0f53ac52748ae1f6b5c
ngugi <aurumjoie45@gmail.com>
2018-04-30 14:41:14 +0300

    init commit

--- srcs/main.js
+++
@@ -1,6 +0,0 @@
-// Create a timeout for welcome alert!
-
-var changeTitle = setTimeout( function() {
-    var title = document.getElementById("subtitle");
-    title.innerHTML = "Time to code!";
-}, 1500 );
